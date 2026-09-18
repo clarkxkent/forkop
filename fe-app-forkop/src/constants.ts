f5a666b44d4667a75c96ffbb5fc7a36b597318be
clarkxkent <astomilov@gmail.com>
2026-09-19 00:10:29 +0500

    Обновление списка DNS серверов

--- fe-app-forkop/src/constants.ts
+++ fe-app-forkop/src/constants.ts
@@ -43,13 +43,16 @@ export const DOMAIN_LIST_OPTIONS = {
 };
 
 export const DNS_SERVER_OPTIONS = {
-  '1.1.1.1': '1.1.1.1 (Cloudflare)',
-  '8.8.8.8': '8.8.8.8 (Google)',
-  '9.9.9.9': '9.9.9.9 (Quad9)',
-  'dns.adguard-dns.com': 'dns.adguard-dns.com (AdGuard Default)',
-  'unfiltered.adguard-dns.com':
-    'unfiltered.adguard-dns.com (AdGuard Unfiltered)',
-  'family.adguard-dns.com': 'family.adguard-dns.com (AdGuard Family)',
+  '77.88.8.8': '77.88.8.8 (Yandex DNS)',
+  '77.88.8.1': '77.88.8.1 (Yandex DNS)',
+  '1.1.1.1': '1.1.1.1 (Cloudflare DNS)',
+  '1.0.0.1': '1.0.0.1 (Cloudflare DNS)',
+  '8.8.8.8': '8.8.8.8 (Google DNS)',
+  '8.8.4.4': '8.8.4.4 (Google DNS)',
+  '9.9.9.9': '9.9.9.9 (Quad9 DNS)',
+  '149.112.112.112': '149.112.112.112 (Quad9 DNS)',
+  '223.5.5.5': '223.5.5.5 (AliDNS)',
+  '223.6.6.6': '223.6.6.6 (AliDNS)',
 };
 export const BOOTSTRAP_DNS_SERVER_OPTIONS = {
   '77.88.8.8': '77.88.8.8 (Yandex DNS)',
@@ -59,7 +62,9 @@ export const BOOTSTRAP_DNS_SERVER_OPTIONS = {
   '8.8.8.8': '8.8.8.8 (Google DNS)',
   '8.8.4.4': '8.8.4.4 (Google DNS)',
   '9.9.9.9': '9.9.9.9 (Quad9 DNS)',
-  '9.9.9.11': '9.9.9.11 (Quad9 DNS)',
+  '149.112.112.112': '149.112.112.112 (Quad9 DNS)',
+  '223.5.5.5': '223.5.5.5 (AliDNS)',
+  '223.6.6.6': '223.6.6.6 (AliDNS)',
 };
 
-export const COMMAND_TIMEOUT = 10000; // 10 seconds
+export const COMMAND_TIMEOUT = 5000; // 5 seconds
